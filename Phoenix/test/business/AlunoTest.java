1ab9e0769a8ab72e53bb5634ac43c67eee48f826
ejpmateus <ejpmateus@885da4b4-edb2-c91c-6a07-c5183dce7a97>
2011-04-28 12:39:32 +0000

    Adicionado: - AbstractFactory diagram

--- Phoenix/test/business/AlunoTest.java
+++ Phoenix/test/business/AlunoTest.java
@@ -1,49 +1,19 @@
 package business;
 
-import org.junit.BeforeClass;
+import junit.framework.TestCase;
+
 import org.junit.Test;
-import business.Aluno;
-import business.CalendarDate;
+import org.junit.runners.model.TestClass;
 
-/**
- * @author Ed Mateus
- * 
- */
-public class AlunoTest extends ScenarioLoader {
+public class AlunoTest extends TestClass{
 
-	public static void toggleAssertions(boolean b) {
-
-		Aluno.class.getClassLoader().setClassAssertionStatus("business.Aluno",
-				b);
-		Pessoa.class.getClassLoader().setClassAssertionStatus(
-				"business.Pessoa", b);
-		
+	public AlunoTest(Class<?> klass) {
+		super(klass);
+		// TODO Auto-generated constructor stub
 	}
 
-	@Test
-	public void testScenarioA() {
-		toggleAssertions(false);
-		loadScenario("data/phenix_A.cmd");
-		toggleAssertions(true);
-
-		CalendarDate date = new CalendarDate(31, 7, 1990);
-		Aluno a1 = (Aluno) Aluno.objectos().toArray()[0];
-
-		a1.setNumero(-1);
-		System.out.println("asdsd");
-		// a1.setDataNascimento(date);
-		a1.setNome("Ed");
-		for (Curso c : Curso.objectos())
-			a1.creditosObtidos(c);
-
-		a1.aprovadas();
+	@Test(expected=AssertionError.class)
+	public void test1(){
+		Aluno a = new Aluno(null, null, "someMail", -1);
 	}
-
-	@Test
-	public void testScenarioB() {
-		loadScenario("data/phenix_B.cmd");
-		CalendarDate date = new CalendarDate(31, 7, 1990);
-		Aluno a1 = (Aluno) Aluno.objectos().toArray()[0];
-	}
-
 }
